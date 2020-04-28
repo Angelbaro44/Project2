a21c6b64dc1ff80c8b80a7ae8c22eddaff1a6735
Angel Baro <angelbaro44@gmail.com>
2020-04-28 08:34:50 -0400

    test

--- cycle-app/src/App.js
+++ cycle-app/src/App.js
@@ -1,26 +1,41 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import 
+import {BrowserRouter,Link,Switch} from 'react-router-dom';
+import axios from 'axios';
+import './Home';
+import './Motorcyle';
+import './Cigar';
+import './Trips';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+ <head></head>
+
+ <header></header>
+
+<div>
+  <div>   <button>   <img src=''  alt=''/>   </button>   </div>
+  <div>   <button>   <img src=''  alt=''/>   </button>   </div>
+  <div>   <button>   <img src=''  alt=''/>   </button>   </div>
+</div>
+<div></div>
+
+<footer></footer>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   );
 }
 
